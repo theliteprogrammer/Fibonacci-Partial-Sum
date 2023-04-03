26b07a115a4738b8e4a7912f652e5879fea19145
theliteprogrammer <219037801@student.uj.ac.za>
2023-04-03 23:39:22 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
+
 public class Main {
+    private static long getFibonacciPartialSumNaive(long from, long to) {
+        long sum = 0;
+
+        long current = 0;
+        long next  = 1;
+
+        for (long i = 0; i <= to; ++i) {
+            if (i >= from) {
+                sum += current;
+            }
+
+            long new_current = next;
+            next = next + current;
+            current = new_current;
+        }
+
+        return sum % 10;
+    }
+
+    /**
+     *
+     * @param from - Starting term
+     * @param to - Ending term
+     * @return The remainder of the sum of fibonacci numbers in the range:  from-to divided by 10
+     */
+    private static long getFibonacciPartialSumFast(long from, long to) {
+        long sum = 0;
+        // create 1D array
+        long[] array = new long[(int)to + 1];
+        array[0] = 0;
+        array[1] = 1;
+        for(int i = 2; i < array.length; i++){
+            array[i] = array[i - 2] + array[i - 1];
+        }
+        for(int i = 0; i < array.length; i++){
+            if(i >= from){
+                sum += array[i];
+            }
+        }
+        return sum % 10;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        long from = scanner.nextLong();
+        long to = scanner.nextLong();
+        System.out.println(getFibonacciPartialSumFast(from, to));
     }
 }
